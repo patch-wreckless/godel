bc07b842f9b8cfe10ee3597414dd116880667e8d
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-02-10 19:05:26 -0400

    feat: implement FieldName.Access
    Implements an Access method for FieldName that dynamically accesses the
    field corresponding to the FieldName on a struct at runtime. This will
    support field-based validations.

--- pkg/fields/fieldname.go
+++ pkg/fields/fieldname.go
@@ -2,6 +2,7 @@ package fields
 
 import (
 	"fmt"
+	"reflect"
 	"regexp"
 )
 
@@ -49,6 +50,41 @@ func MustFieldName(name string) FieldName {
 // String formats the [FieldName] in the dot-notation used for field access.
 func (f FieldName) String() string {
 	return "." + string(f.name)
+}
+
+// Access applies the [FieldName] to the target value as an access expression
+// and returns the field value and true if it was applicable, otherwise false.
+func (f FieldName) Access(target any) (any, bool) {
+
+	valType := reflect.TypeOf(target)
+	for valType.Kind() == reflect.Pointer {
+		valType = valType.Elem()
+	}
+
+	if valType.Kind() != reflect.Struct {
+		return nil, false
+	}
+
+	field, ok := valType.FieldByName(f.name)
+	if !ok {
+		return nil, false
+	}
+
+	if !field.IsExported() {
+		return nil, false
+	}
+
+	val := reflect.ValueOf(target)
+	for val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	if val.Kind() == reflect.Invalid {
+		// The field access was valid for the value type, but there is no value
+		// to get a field from.
+		return nil, true
+	}
+
+	return val.Field(field.Index[0]).Interface(), true
 }
 
 var validFieldNamePattern = regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`)
--- pkg/fields/fieldname_test.go
+++ pkg/fields/fieldname_test.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+
+	"github.com/patch-wreckless/godel/internal/ptr"
 )
 
 func TestFieldName(t *testing.T) {
@@ -103,6 +105,180 @@ func TestFieldName(t *testing.T) {
 		if actual != expected {
 			t.Errorf("expected %q; got %q", expected, actual)
 		}
+	})
+
+	t.Run("#Access", func(t *testing.T) {
+
+		t.Run("inapplicable to target type/returns not ok",
+			func(t *testing.T) {
+
+				testCases := []struct {
+					name  string
+					value any
+				}{
+					{name: "scalar", value: "string"},
+					{name: "slice", value: []string{"slice"}},
+					{name: "map", value: map[string]string{"key": "val"}},
+
+					{name: "pointer to scalar", value: ptr.To("string")},
+					{name: "pointer to slice", value: &[]string{"slice"}},
+					{name: "pointer to map", value: &map[string]string{"key": "val"}},
+
+					{name: "pointer to pointer to scalar", value: ptr.To(ptr.To("string"))},
+				}
+
+				for _, tc := range testCases {
+
+					t.Run(tc.name, func(t *testing.T) {
+						underTest := MustFieldName("Foo")
+						val, ok := underTest.Access(tc.value)
+						if ok {
+							t.Errorf("expected not ok; got (%+v, true)", val)
+						}
+					})
+				}
+			})
+
+		t.Run("target struct has no matching/returns not ok",
+			func(t *testing.T) {
+				target := struct{}{}
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if ok {
+					t.Errorf("expected not ok; got (%+v, true)", val)
+				}
+			})
+
+		t.Run("target struct has matching unexpported field/returns not ok",
+			func(t *testing.T) {
+				target := struct{ foo string }{
+					foo: "unexpected",
+				}
+				underTest := MustFieldName("foo")
+				val, ok := underTest.Access(target)
+				if ok {
+					t.Errorf("expected not ok; got (%+v, true)", val)
+				}
+			})
+
+		t.Run("target struct has matching exported field/returns field value",
+			func(t *testing.T) {
+				expected := "expected"
+				target := struct{ Foo string }{
+					Foo: expected,
+				}
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				actual, ok := val.(string)
+				if !ok {
+					t.Fatalf("expected %T; got %T", expected, val)
+				}
+				if actual != expected {
+					t.Errorf("expected %q; got %q", expected, actual)
+				}
+			})
+
+		t.Run("matching field is nil pointer/returns typed nil",
+			func(t *testing.T) {
+				var expected *string
+				target := struct{ Foo *string }{
+					Foo: expected,
+				}
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				actual, ok := val.(*string)
+				if !ok {
+					t.Fatalf("expected %T; got %T", expected, val)
+				}
+				if actual != expected {
+					t.Errorf("expected %p; got %p", expected, actual)
+				}
+			})
+
+		t.Run("target is nil pointer to struct/returns untyped nil ok",
+			func(t *testing.T) {
+				var target *struct{ Foo string }
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				if val != nil {
+					t.Errorf("expected nil; got %+v", val)
+				}
+			})
+
+		t.Run("target is non-nil pointer to struct/returns field value",
+			func(t *testing.T) {
+				expected := "expected"
+				target := &struct{ Foo string }{
+					Foo: expected,
+				}
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				actual, ok := val.(string)
+				if !ok {
+					t.Fatalf("expected %T; got %T", expected, val)
+				}
+				if actual != expected {
+					t.Errorf("expected %q; got %q", expected, actual)
+				}
+			})
+
+		t.Run("target is nil pointer to pointer to struct/returns untyped nil ok",
+			func(t *testing.T) {
+				var target **struct{ Foo string }
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				if val != nil {
+					t.Errorf("expected nil; got %+v", val)
+				}
+			})
+
+		t.Run("target is non-nil pointer to nil pointer to struct/returns untyped nil ok",
+			func(t *testing.T) {
+				target := ptr.To((*struct{ Foo string })(nil))
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				if val != nil {
+					t.Errorf("expected nil; got %+v", val)
+				}
+			})
+
+		t.Run("target is non-nil pointer to non-nil pointer to struct/returns field value",
+			func(t *testing.T) {
+				expected := "expected"
+				target := ptr.To(&struct{ Foo string }{
+					Foo: expected,
+				})
+				underTest := MustFieldName("Foo")
+				val, ok := underTest.Access(target)
+				if !ok {
+					t.Error("expected ok to be true; got false")
+				}
+				actual, ok := val.(string)
+				if !ok {
+					t.Fatalf("expected %T; got %T", expected, val)
+				}
+				if actual != expected {
+					t.Errorf("expected %q; got %q", expected, actual)
+				}
+			})
 	})
 }
 
