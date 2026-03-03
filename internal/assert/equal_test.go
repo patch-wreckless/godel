873f96b7dd509a9f18399efd1a6aeab359ec1aaa
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-03-03 11:54:42 -0400

    chore: negative test for interface values eq
    Adds a negative test case for Equal on interface values.
    
    Restructures the tests to isolate the configuration scenarios when
    reporting errors.

--- internal/assert/equal_test.go
+++ internal/assert/equal_test.go
@@ -6,27 +6,37 @@ import (
 
 func TestEqual(t *testing.T) {
 
-	t.Run("comparable values are equal/does not report error", func(t *testing.T) {
-		mockT := newMockTestingT(t)
-		Equal(mockT, 5, 5)
-	})
+	t.Run("comparable values", func(t *testing.T) {
 
-	t.Run("interface values are equal/does not report error", func(t *testing.T) {
-		mockT := newMockTestingT(t)
-		var x any = "six"
-		var y any = "six"
-		Equal(mockT, x, y)
-	})
+		t.Run("are equal/does not report error", func(t *testing.T) {
+			mockT := newMockTestingT(t)
+			Equal(mockT, 5, 5)
+		})
 
-	t.Run("values are not equal", func(t *testing.T) {
-
-		t.Run("reports error", func(t *testing.T) {
+		t.Run("are not equal/reports error", func(t *testing.T) {
 			mockT := newMockTestingT(t)
 			mockT.Expect().Errorf("expected %v; got %v", 5, 4)
 			Equal(mockT, 5, 4)
 		})
+	})
 
-		t.Run("expression configured/reports error with expression", func(t *testing.T) {
+	t.Run("interface values", func(t *testing.T) {
+
+		t.Run("are equal/does not report error", func(t *testing.T) {
+			mockT := newMockTestingT(t)
+			Equal(mockT, "six", "six")
+		})
+
+		t.Run("are not equal/reports error", func(t *testing.T) {
+			mockT := newMockTestingT(t)
+			mockT.Expect().Errorf("expected %v; got %v", "six", "half of a dozen")
+			Equal(mockT, "six", "half of a dozen")
+		})
+	})
+
+	t.Run("reporting error", func(t *testing.T) {
+
+		t.Run("with expression configured/error includes expression", func(t *testing.T) {
 			mockT := newMockTestingT(t)
 			mockT.Expect().Errorf("expected %s to be %v; got %v", "value", 5, 4)
 			Equal(mockT, 5, 4, func(ec *EqualConf[int]) {
